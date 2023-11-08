cd9b91b3284efa2d102f187948a2570ef25d94fa
waleed tarazi <waleedaltarazy@gmail.com>
2023-11-08 17:42:40 +0300

    add/Patient class

---
+++ java_project/src/patients/Patient.java
@@ -0,0 +1,16 @@
+package patients;
+
+public class Patient {
+    int id;
+    String name;
+    int age;
+    String status;
+
+
+    public Patient(){
+        this.id = 0;
+        this.name = "no name";
+        this.age = 0;
+    }
+    
+}
