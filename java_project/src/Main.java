0641eca16f5d4a0ea764cd263788fb853a147cb3
Cup-Coding <info@cupcoding.com>
2023-11-08 17:58:51 +0200

    update from Dell

--- java_project/src/Main.java
+++ java_project/src/Main.java
@@ -3,7 +3,6 @@ import patients.Patient;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
-    Hospital hospital1 = new Hospital();  
     Patient patient1 = new Patient();  
     }
     }
